262a02e06dffe017d1b85895bfaf4ba847ee7610
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-03-10 06:26:42 +0700

    refactor(task): Add guard with bearer token jwt for all task endpoins

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,9 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -20,21 +22,25 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
+  @UseGuards(AuthGuard())
   getTasks(@Query() getTasksFilterDto: GetTasksFilterDto): Promise<Task[]> {
     return this.tasksService.getAllTasks(getTasksFilterDto);
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard())
   getTaskById(@Param('id') id: string): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard())
   createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }
 
   @Patch('/:id')
+  @UseGuards(AuthGuard())
   updateTaskById(
     @Param('id') id: string,
     @Body() updateTaskDto: UpdateTaskDto,
@@ -43,6 +49,7 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
+  @UseGuards(AuthGuard())
   updateTaskStatusById(
     id: string,
     @Body() updateTaskStatusById: UpdateTaskStatusDto,
@@ -51,6 +58,7 @@ export class TasksController {
   }
 
   @Delete('/:id')
+  @UseGuards(AuthGuard())
   deleteTask(@Param('id') id: string): Promise<void> {
     return this.tasksService.deleteTaskById(id);
   }
