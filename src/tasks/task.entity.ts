53ec69bd7ef194259bd7ecbe34f0210d95f5f378
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-03-12 18:59:26 +0700

    refactor(task): Remove unused variable

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -28,7 +28,7 @@ export class Task {
   })
   status: TaskStatus;
 
-  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @ManyToOne(() => User, (user) => user.tasks, { eager: false })
   @Exclude({ toPlainOnly: true })
   user: User;
 }
